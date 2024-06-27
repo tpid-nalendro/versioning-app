41b8982df38ac5308c5629836a94c674d27d7087
tpid-nalendro <nalendro@technopartner.co.id>
2024-06-27 11:02:38 +0700

    feat: add version to hard reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { images } from './assets/images'
+import axios from 'axios'
 
 function App() {
+  const [currentVersion, setCurrentVersion] = useState(null)
+
+  useEffect(() => {
+    // Function to fetch the version from version.json
+    const fetchVersion = async () => {
+      try {
+        const response = await axios.get('/version.json')
+        const latestVersion = response.data.version
+
+        if (currentVersion && currentVersion !== latestVersion) {
+          window.location.reload() // true parameter forces hard reload
+        } else {
+          setCurrentVersion(latestVersion)
+        }
+      } catch (error) {
+        console.error('Error fetching version:', error)
+      }
+    }
+
+    const intervalId = setInterval(fetchVersion, 60000)
+
+    fetchVersion()
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId)
+  }, [currentVersion])
+
   return (
     <div className='App'>
       <header className='App-header'>
