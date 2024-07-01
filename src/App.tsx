f84fc8aeef33f902728bad4fce1f63a3bf43b32f
tpid-nalendro <nalendro@technopartner.co.id>
2024-07-01 15:51:33 +0700

    fix: build version only on prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { useEffect } from 'react'
 import './App.css'
 import { images } from './assets/images'
 import useVersioning from './hooks/versioning'
 
 export default function App() {
-  useVersioning('/version.json')
+  console.log(useVersioning('/version.json'))
 
   return (
     <div className='App'>
