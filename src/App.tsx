1b68b8f07d7bfe3f3c0f3aaecda515d6d39026ce
tpid-nalendro <nalendro@technopartner.co.id>
2024-07-01 16:09:00 +0700

    fix: build version only on prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { images } from './assets/images'
 import useVersioning from './hooks/versioning'
 
 export default function App() {
-  const [currentVersion, setCurrentVersion] = useState(null)
-
-  useEffect(() => {
-    const fetchVersion = async () => {
-      try {
-        const response = await fetch('/version.json').then((data) => {
-          return data.json()
-        })
-        const latestVersion = response.version
-
-        if (currentVersion && currentVersion !== latestVersion) {
-          console.log(currentVersion, latestVersion, 'must reload here')
-          window.location.reload()
-        } else {
-          setCurrentVersion(latestVersion)
-        }
-      } catch (error) {
-        console.error('Error fetching version:', error)
-      }
-    }
-    fetchVersion()
-
-    const intervalId = setInterval(fetchVersion, 10 * 1000)
-    return () => clearInterval(intervalId)
-  }, [])
+  useVersioning('/version.json')
 
   return (
     <div className='App'>
