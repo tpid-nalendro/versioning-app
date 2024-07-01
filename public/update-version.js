2082f8946cab1af90fa4cd3b63298823aa69695e
tpid-nalendro <nalendro@technopartner.co.id>
2024-07-01 14:57:41 +0700

    fix: version auto increment fix

--- public/update-version.js
+++ public/update-version.js
@@ -12,11 +12,20 @@ fs.readFile(filePath, 'utf8', (err, data) => {
   const versionData = JSON.parse(data)
   const versionParts = versionData.version.split('.').map(Number)
 
-  versionParts[versionParts.length - 1] += 1
+  versionParts[2] += 1
+
+  if (versionParts[2] === 10) {
+    versionParts[2] = 0
+    versionParts[1] += 1
+
+    if (versionParts[1] === 10) {
+      versionParts[1] = 0
+      versionParts[0] += 1
+    }
+  }
 
   versionData.version = versionParts.join('.')
 
-  console.log(versionData)
   fs.writeFile(
     filePath,
     JSON.stringify(versionData, null, 2),
@@ -26,7 +35,13 @@ fs.readFile(filePath, 'utf8', (err, data) => {
         console.error('Error writing version.json:', err)
         return
       }
-      console.log('Version incremented successfully.')
+      console.log(
+        `Version incremented successfully to ${JSON.stringify(
+          versionData,
+          null,
+          2
+        )}`
+      )
     }
   )
 })
--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
           alt='logo'
           style={{ marginBottom: '4px' }}
         />
-        must be 0.0.3 version:{version}
+        Version:{version}
       </header>
     </div>
   )
